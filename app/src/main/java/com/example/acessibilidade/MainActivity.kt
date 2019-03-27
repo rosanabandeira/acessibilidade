c7df0729f77c0cdbb70a64c5e1a3c3d0d89083ba
Rosana Bandeira <rosanabandeira@nbbv027697.local>
2019-03-27 16:58:43 -0300

    commit

--- app/src/main/java/com/example/acessibilidade/MainActivity.kt
+++ app/src/main/java/com/example/acessibilidade/MainActivity.kt
@@ -12,6 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var actionBar = supportActionBar
+        actionBar!!.setDisplayHomeAsUpEnabled(true)
 
     }
 }
