e0b35702f87f24be3f076d8cfac4be42091ba9e2
Rosana Bandeira <rosanabandeira@nbbv027697.local>
2019-03-27 15:43:21 -0300

    first commit

---
+++ app/src/main/java/com/example/acessibilidade/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.acessibilidade
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.view.ViewCompat
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
